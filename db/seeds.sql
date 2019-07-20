b01cff6b2cccd8b432fb5b2786dedad456fa9793
LM Train <layconmm@msn.com>
2019-07-20 13:42:40 -0500

    Create line codes

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Beacon Burger');
+INSERT INTO burgers (name, eaten) VALUES ('Chicken Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Pork Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (name) VALUES ('Fish Burger');
