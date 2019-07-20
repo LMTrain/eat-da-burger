db04189552bb291b9ff5e9234fc8f47d8ce08fe0
LM Train <layconmm@msn.com>
2019-07-20 14:15:52 -0500

    Changed folder name and add line of codes to packagejson and scheema

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
-### Schema
+CREATE DATABASE eatda_burger_db;
 
-CREATE DATABASE cat_db;
-USE cat_db;
+-- Use the DB wizard_schools_db for all the rest of the script
+USE eatda_burger_db;
 
-CREATE TABLE cats
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+-- Created the table "schools"
+CREATE TABLE burgers (
+  id int AUTO_INCREMENT NOT NULL,
+  name varchar(50) NOT NULL,
+  eaten BOOLEAN DEFAULT false,
+  PRIMARY KEY(id)
 );
